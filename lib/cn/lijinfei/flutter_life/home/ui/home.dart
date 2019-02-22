1d00afce668e25da6c9ca07cb60a9ad178082f0f
Bartholomew <bart@BartdeMacBook-Pro.local>
2019-02-22 12:19:01 +0800

    no message

--- lib/cn/lijinfei/flutter_life/home/ui/home.dart
+++ lib/cn/lijinfei/flutter_life/home/ui/home.dart
@@ -26,9 +26,36 @@ class HomeWidget extends StatelessWidget{
                 AsyncSnapshot<Weather> weather) {
               print(weather);
               if(weather.data!=null){
+
                 String result = "今天是"+weather.data.date+weather.data.week+",\n"+weather.data.city+"的天气为"+weather.data.weather+",\n"+weather.data.wind+"，\n气温为"+weather.data.temperature
-              +weather.data.washIndex+"擦洗车辆,\n"+weather.data.humidity+",\n"+"空气质量"+weather.data.airCondition;
-                return Text(result);
+              +"\n"+weather.data.washIndex+"擦洗车辆,\n"+weather.data.humidity+",\n"+"空气质量"+weather.data.airCondition;
+                String weatherImageName = "assets/images/fine.png";
+                if(weather.data.weather == "晴"){
+                  weatherImageName = "assets/images/fine.png";
+                }else if(weather.data.weather.contains(new RegExp('阴'))||weather.data.weather.contains(new RegExp('云'))){
+                  weatherImageName = "assets/images/cloudy.png";
+                }else if(weather.data.weather == "多云转晴"){
+                  weatherImageName = "assets/images/cloudy_becoming_fine.png";
+                }else if(weather.data.weather.contains(new RegExp('雷'))){
+                  weatherImageName = "assets/images/thunderstorm.png";
+                }else if(weather.data.weather.contains(new RegExp('雪'))){
+                  weatherImageName = "assets/images/sonw.png";
+                }
+
+                Image image = new Image(
+                    image: new AssetImage(weatherImageName),width: 100.0,height: 100.0,
+              // ...
+              );
+                Column column = new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+
+                    new Padding(padding: new EdgeInsets.all(10.0), child: image),
+                    new Text(result)
+                  ],
+                );
+
+                return column;
               }else{
                 return Text("");
               }
